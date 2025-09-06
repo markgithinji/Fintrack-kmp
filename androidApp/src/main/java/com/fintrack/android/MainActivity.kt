151fe4817263d634503924e73bb338d27b6d8c24
markgithinji <57723258+markgithinji@users.noreply.github.com>
2025-09-06 23:40:49 +0300

    Initial commit

---
+++ androidApp/src/main/java/com/fintrack/android/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.fintrack.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.fintrack.app.App
+
+//import com.fintrack.app.App
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            App()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AppAndroidPreview() {
+    App()
+}
---
+++ shared/src/commonMain/kotlin/com/fintrack/app/Greeting.kt
@@ -0,0 +1,9 @@
+package com.fintrack.app
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
