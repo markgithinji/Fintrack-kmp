612da2d660badd282ba25c8a4f4b1d533063037f
markgithinji <57723258+markgithinji@users.noreply.github.com>
2025-10-08 14:58:37 +0300

    refactor(AccountViewmodel): Simplify `reloadAccounts` logic

--- shared/src/commonMain/kotlin/com/fintrack/shared/feature/account/ui/AccountViewmodel.kt
+++ shared/src/commonMain/kotlin/com/fintrack/shared/feature/account/ui/AccountViewmodel.kt
@@ -31,18 +31,20 @@ class AccountsViewModel(private val repo: AccountRepository) : ViewModel() {
     fun reloadAccounts() {
         viewModelScope.launch {
             _accounts.value = Result.Loading
-            _selectedAccount.value = Result.Loading
 
             val result = repo.getAccounts()
             _accounts.value = result
 
-            if (result is Result.Success && result.data.isNotEmpty()) {
-                // pick first account by default
-                _selectedAccount.value = Result.Success(result.data.first())
-            } else if (result is Result.Success && result.data.isEmpty()) {
-                _selectedAccount.value = Result.Error(Exception("No accounts available"))
-            } else if (result is Result.Error) {
-                _selectedAccount.value = result
+            when (result) {
+                is Result.Success -> {
+                    if (result.data.isNotEmpty()) {
+                        _selectedAccount.value = Result.Success(result.data.first())
+                    } else {
+                        _selectedAccount.value = Result.Error(Exception("No accounts available"))
+                    }
+                }
+                is Result.Error -> _selectedAccount.value = result
+                else -> Unit
             }
         }
     }
